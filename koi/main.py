4eea1c1f4059578e20a25b7b6430d32a6d8e472f
Kye <kye@apacmediasolutions.com>
2023-09-11 14:05:30 -0400

    clean up  `Sakana`

--- koi/main.py
+++ koi/main.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 from copy import deepcopy
 
-
 class Koi:
     def __init__(
         self,
@@ -59,8 +58,42 @@ class Koi:
         return self.model(x)
     
 class Sakana:
-    def __init__(self, num_koi, model, step_size, num_steps, num_iterations):
-        self.kois = [Koi(deepcopy(model), step_size, num_steps, num_iterations) for _ in range(num_koi)]
+    """
+    # Initialize the model
+    model = nn.Sequential(
+        nn.Linear(1, 64),
+        nn.Tanh(),
+        nn.Linear(64, 64),
+        nn.Tanh(),
+        nn.Linear(64, 1),
+    )
+
+    # Create a random torch input
+    x = torch.randn(1, 1)
+
+    # Initialize the swarm
+    sakana = Sakana(num_koi=10, model=model, step_size=0.01, num_steps=10, num_iterations=1000)
+
+    # Perform a forward pass through the swarm
+    outputs = sakana.forward(x)
+
+    # Print the outputs
+    for i, output in enumerate(outputs):
+        print(f"Output of Koi instance {i}: {output.item()}")
+    """
+
+    def __init__(
+            self,
+            num_koi, 
+            model, 
+            step_size, 
+            num_steps, 
+            num_iterations
+        ):
+        self.kois = [
+            Koi(
+                deepcopy(model), step_size, num_steps, num_iterations) for _ in range(num_koi)
+            ]
         self.best_positions = [koi.model.state_dict() for koi in self.kois]
         self.best_swarm_position = deepcopy(self.best_positions[0])
         self.velocities = [0 for _ in range(num_koi)]
@@ -68,30 +101,11 @@ class Sakana:
     def update_positions(self):
         for i, koi in enumerate(self.kois):
             for param in koi.model.parameters():
-                self.velocities[i] += 0.1 * (self.best_positions[i] - param.data) + 0.1 * (self.best_swarm_position - param.data)
+                self.velocities[i] += 0.1 * (
+                    self.best_positions[i] - param.data
+                ) + 0.1 * (self.best_swarm_position - param.data)
+
                 param.data += self.velocities[i]
 
     def forward(self, x):
         return [koi.forward(x) for koi in self.kois]
-
-# Initialize the model
-model = nn.Sequential(
-    nn.Linear(1, 64),
-    nn.Tanh(),
-    nn.Linear(64, 64),
-    nn.Tanh(),
-    nn.Linear(64, 1),
-)
-
-# Create a random torch input
-x = torch.randn(1, 1)
-
-# Initialize the swarm
-sakana = Sakana(num_koi=10, model=model, step_size=0.01, num_steps=10, num_iterations=1000)
-
-# Perform a forward pass through the swarm
-outputs = sakana.forward(x)
-
-# Print the outputs
-for i, output in enumerate(outputs):
-    print(f"Output of Koi instance {i}: {output.item()}")
